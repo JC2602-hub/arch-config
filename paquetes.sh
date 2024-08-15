52eabcd51f9b10e98424bedcb2b5b72dfcd7808e
Juan <ponceortegajuan@gmail.com>
2024-08-14 22:13:34 -0300

    Organizacion

--- paquetes.sh
+++ paquetes.sh
@@ -1,11 +1,15 @@
-!/bin/bash
+/bin/bash
 
 # Actualizar la lista de paquetes y el sistema
 pacman -Syu --noconfirm
 
-# Instalar
+# Instalar paquetes basicos mas KDE pre-requisitos
 #
-pacman -Syu --noconfirm ntfs-3g linux-headers base-devel networkmanager network-manager-applet wpa_supplicant wireless_tools dialog cups cups-pdf xdg-utils xdg-user-dirs noto-fonts print-manager reflector ttf-liberation ttf-ubuntu-font-family ttf-anonymous-pro ttf-bitstream-vera adobe-source-sans-pro-fonts noto-fonts-cjk noto-fonts-emoji gufw ttf-croscore ttf-dejavu ttf-ibm-plex ttf-jetbrains-mono ttf-liberation xterm unzip unrar p7zip baloo-widgets dolphin-plugins ffmpegthumbs kde-inotify-survey kdeconnect kdegraphics-thumbnailers kdenetwork-filesharing kimageformats kio-admin kio-extras kio-fuse kio-gdrive libappindicator-gtk3 phonon-qt6-vlc qt6-imageformats xwaylandvideobridge icoutils iio-sensor-proxy maliit-keyboard power-profiles-daemon xdg-desktop-portal-gtk xsettingsd
+pacman -Syu --noconfirm ntfs-3g linux-headers base-devel networkmanager network-manager-applet wpa_supplicant wireless_tools dialog cups cups-pdf xdg-utils xdg-user-dirs print-manager reflector gufw xterm unzip unrar p7zip baloo-widgets dolphin-plugins ffmpegthumbs kde-inotify-survey kdeconnect kdegraphics-thumbnailers kdenetwork-filesharing kimageformats kio-admin kio-extras kio-fuse kio-gdrive libappindicator-gtk3 phonon-qt6-vlc qt6-imageformats xwaylandvideobridge icoutils iio-sensor-proxy maliit-keyboard power-profiles-daemon xdg-desktop-portal-gtk xsettingsd
+
+# Instalar fuentes
+#
+pacman -S --noconfirm noto-fonts ttf-liberation ttf-ubuntu-font-family ttf-anonymous-pro ttf-bitstream-vera adobe-source-sans-pro-fonts noto-fonts-cjk noto-fonts-emoji ttf-croscore ttf-dejavu ttf-ibm-plex ttf-jetbrains-mono ttf-liberation cantrell-fonts inter-font ttf-caladea ttf-carlito ttf-cascadia-code ttf-droid ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-inconsolata ttf-opensans ttf-roboto
 
 # Habilatar servicios
 #
@@ -14,6 +18,7 @@ systemctl enable cups.service
 systemctl enable fstrim.timer
 systemctl enable power-profiles-daemon
 systemctl enable reflector.timer
+systemctl enable ufw.service
 
 # Instalar bootloader
 #
