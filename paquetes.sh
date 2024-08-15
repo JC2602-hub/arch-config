ba15c10682b2747ad2ea179affc68b72d176a56f
Juan <ponceortegajuan@gmail.com>
2024-08-14 22:53:42 -0300

    Pulido 1

--- paquetes.sh
+++ paquetes.sh
@@ -5,7 +5,7 @@ pacman -Syu --noconfirm
 
 # Instalar paquetes basicos mas KDE pre-requisitos
 #
-pacman -Syu --noconfirm ntfs-3g linux-headers base-devel networkmanager network-manager-applet wpa_supplicant wireless_tools dialog cups cups-pdf xdg-utils xdg-user-dirs print-manager reflector gufw xterm unzip unrar p7zip baloo-widgets dolphin-plugins ffmpegthumbs kde-inotify-survey kdeconnect kdegraphics-thumbnailers kdenetwork-filesharing kimageformats kio-admin kio-extras kio-fuse kio-gdrive libappindicator-gtk3 phonon-qt6-vlc qt6-imageformats xwaylandvideobridge icoutils iio-sensor-proxy maliit-keyboard power-profiles-daemon xdg-desktop-portal-gtk xsettingsd
+pacman -S --noconfirm ntfs-3g linux-headers base-devel networkmanager network-manager-applet wpa_supplicant wireless_tools dialog cups cups-pdf xdg-utils xdg-user-dirs print-manager reflector gufw xterm unzip unrar p7zip baloo-widgets dolphin-plugins ffmpegthumbs kde-inotify-survey kdeconnect kdegraphics-thumbnailers kdenetwork-filesharing kimageformats kio-admin kio-extras kio-fuse kio-gdrive libappindicator-gtk3 phonon-qt6-vlc qt6-imageformats xwaylandvideobridge icoutils iio-sensor-proxy maliit-keyboard power-profiles-daemon xdg-desktop-portal-gtk xsettingsd
 
 # Instalar fuentes
 #
@@ -26,7 +26,7 @@ pacman -S --noconfirm grub mtools dosfstools os-prober
 
 grub-install --target=i386-pc --recheck /dev/sda
 
-grub-mkconfig - o /boot/grub/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
 
 # Instalar drivers graficos
 #
@@ -34,7 +34,7 @@ pacman -S mesa libva-intel-driver libva-utils
 
 # Instalar Plasma
 #
-pacman -S --noconfirm plasma kde-system-meta kde-utilities-meta kde-sdk-meta kde-graphics-meta git pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack alsa-utils packagekit-qt6 discord telegram-desktop calibre firefox firefox-i18n-es-ar libreoffice-fresh libreoffice-fresh-es hunspell hunspell-es_any hyphen hyphen-es
+pacman -S --noconfirm plasma-meta kde-system-meta kde-utilities-meta kde-sdk-meta kde-graphics-meta git pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack alsa-utils packagekit-qt6 discord telegram-desktop calibre firefox firefox-i18n-es-ar libreoffice-fresh libreoffice-fresh-es hunspell hunspell-es_any hyphen hyphen-es
 
 systemctl enable sddm
 
